5c7baee7e60bf533d8f0edf04b9ebae665c65ef2
ilmerry <milkiss0414@naver.com>
2021-08-03 17:36:53 +0900

    result.css correction

--- scripts/result.js
+++ scripts/result.js
@@ -4,22 +4,22 @@ const restartButton = document.querySelector(".js-restart"),
     resultExplain = document.querySelector(".js-resultExplain");
 
 const title_LIST = {
-    "INTJ" : "타이틀",
-    "ESTJ" : "설명",
-    "INFJ" : "설명",
-    "ISFJ" : "설명",
-    "ISTJ" : "설명",
-    "ISFP" : "설명",
-    "INFP" : "설명",
-    "ENFJ" : "설명",
-    "ISTP" : "설명",
-    "INIP" : "설명",
-    "ENTJ" : "설명",
-    "ENFP" : "설명",
-    "ENTP" : "설명",
-    "ESTP" : "설명",
-    "ESFP" : "설명",
-    "ESFJ" : "설명"
+    "INTJ" : "조직의 우두머리",
+    "ESTJ" : "조직의 2인자",
+    "INFJ" : "조직의 후계자",
+    "ISFJ" : "조직의 서포터",
+    "ISTJ" : "조직의 감시자",
+    "ISFP" : "조직의 이중스파이",
+    "INFP" : "조직의 중재자",
+    "ENFJ" : "조직의 통솔자",
+    "ISTP" : "조직의 설계자",
+    "INTP" : "조직의 독설가",
+    "ENTJ" : "조직의 야망가",
+    "ENFP" : "조직의 반항아",
+    "ENTP" : "조직의 언변가",
+    "ESTP" : "조직의 행동대장",
+    "ESFP" : "조직의 분위기메이커",
+    "ESFJ" : "조직의 대모"
 };
 const expain_LIST = [
     "설명1",
@@ -43,17 +43,17 @@ const expain_LIST = [
 const USER_MBTI = "currentMBTI";
 
 function handleRestart(){
-    const link = '/question.html';
+    const link = '/questions.html';
     location.href = link;
 }
 
 function resultGreeting(){
     let index = 0;
-    const currentMBTI = localStorage.getItem(USER_MBTI);
+    const userMBTI = localStorage.getItem(USER_MBTI);
 
-    resultImg.src = "/images/" + currentMBTI +".png";
+    resultImg.src = "/images/" + userMBTI +".png";
     for (var mbti in title_LIST){
-        if(mbti == currentMBTI){
+        if(mbti == userMBTI){
             resultTitle.innerText = title_LIST[mbti];
             resultExplain.innerText = expain_LIST[index];
             return;
